f06d24c3ec706801c21c2e0a7a64971c6f53c284
Ron Yaari <ryaari@salesforce.com>
2024-11-16 20:14:40 +0200

    Support comments in posts

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -1,4 +1,6 @@
 const Post = require('../models/post.model');
+const mongoose = require("mongoose");
+const Comment = require("../models/comment.model");
 
 const postController = {
   async getAllPosts(req, res) {
@@ -18,7 +20,7 @@ const postController = {
   },
   async getPostById(req, res) {
     try {
-      const post = await Post.findById(req.params.id);
+      const post = await Post.findById(req.params.id).populate('comments');
       if (!post) return res.status(404).json({ error: "Post not found" });
       res.json(post);
     } catch (error) {
@@ -44,12 +46,26 @@ const postController = {
     }
   },
   async deletePost(req, res) {
+    const session = await mongoose.startSession();
+    session.startTransaction();
+
     try {
-      const post = await Post.findByIdAndDelete(req.params.id);
-      if (!post) return res.status(404).json({ error: "Post not found" });
+      const { id } = req.params;
+      const post = await Post.findByIdAndDelete(id).session(session);
+      if (!post) {
+        await session.abortTransaction();
+        return res.status(404).json({ error: "Post not found" });
+      }
+      await Comment.deleteMany({ postId: id }).session(session);
+      await post.deleteOne({ session });
+      await session.commitTransaction();
       res.json({ message: "Post deleted successfully" });
     } catch (error) {
+      await session.abortTransaction();
       res.status(500).json({ error: error.message });
+    }
+    finally {
+      session.endSession();
     }
   }
 };
